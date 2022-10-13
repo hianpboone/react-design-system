8e3d4db6182c0dcc5274e42d4622e2ca05179e7f
Hian Boone <hianpboone@hotmail.com>
2022-10-12 23:42:44 -0300

    Initial commit

---
+++ storybook-static/assets/preview.c4a49c5f.js
@@ -0,0 +1,2 @@
+import{t as o}from"./index.b17d894c.js";import"./iframe.12bbf2e6.js";import"./es.map.constructor.04559a77.js";import"./es.number.to-fixed.46fc39df.js";import"./index.83bc1c56.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
+//# sourceMappingURL=preview.c4a49c5f.js.map
---
+++ tailwind.config.cjs
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.tsx',
+  ],
+  theme: {
+
+    fontSize: {
+      xs: 14,
+      sm: 16,
+      md: 18,
+      lg: 20,
+      xl: 24,
+      '2xl': 32,
+    },
+
+    colors: {
+      gray:{
+        900:'#121214',
+        800:'#202024',
+        400:'#7C7C7C',
+        100:'#E1E1E6',
+      },
+
+      cyan: {
+        500:'#81D8F7',
+        400:'#94E3FF',
+      },
+
+      'black': '#000000',
+      'white': '#FFFFFF',
+      'transparent': 'transparent',
+    },
+    extend: {
+      fontFamily: {
+        sans: 'Inter, sans-serif'
+      }
+    },
+  },
+  plugins: [],
+}
